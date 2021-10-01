5681fbbb05740e1b63ffa0af4a4425f76199b201
hamzah79 <hamzah.pujiningrat@gmail.com>
2021-10-01 13:37:57 +0700

    Update kodemu.js

--- kodemu.js
+++ kodemu.js
@@ -2,7 +2,7 @@ var timeleft = 45;
 var downloadTimer = setInterval(function(){
   if(timeleft <= 0){
     clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "KODEMU";
+    document.getElementById("countdown").innerHTML = "JENDRAWINA";
   } else {
     document.getElementById("countdown").innerHTML = timeleft + " seconds Code";
   }
