b78a4f16b3a889ce569eb7f60cede519248c68fc
hamzah79 <hamzah.pujiningrat@gmail.com>
2021-10-01 14:21:37 +0700

    Update splitblog.js

--- splitblog.js
+++ splitblog.js
@@ -1,11 +1,9 @@
-var ars 	= rChoice([
-	'https://rezfoods.com/',
-	'https://topic.rezfoods.com/',
-	]);
-
+var ars = rChoice([
+'http://rezfoods.com.com/',
+'http://topic.rezfoods.com/'
+]);
 function rChoice(arr) {
-    return arr[Math.floor(arr.length * Math.random())];
+return arr[Math.floor(arr.length * Math.random())];
 }
-
 //Code arsae client javascript
-if(['.google.', 'bing.', 't.co', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+if(['.google.', 'bing.', '.yahoo.', 'yandex.', 't.co', 'facebook.', 'duckduckgo.', 'instagram.', 'twitter.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
