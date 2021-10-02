c0a5eadd9e4d923ed8d43f1158711cac489bdf85
hamzah79 <hamzah.pujiningrat@gmail.com>
2021-10-02 13:43:45 +0700

    Create codebottom.js

---
+++ codebottom.js
@@ -0,0 +1,10 @@
+var timeleft = 100;
+var downloadTimer = setInterval(function(){
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "NYIRAM-BOT6647";
+  } else {
+    document.getElementById("countdown").innerHTML = timeleft + " seconds Code";
+  }
+  timeleft -= 1;
+}, 1000);
