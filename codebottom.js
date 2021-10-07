c4d3b9e379d6d70b8ed1d1ee306257eb95c0fd6a
hamzah79 <hamzah.pujiningrat@gmail.com>
2021-10-07 12:22:20 +0700

    Update codebottom.js

--- codebottom.js
+++ codebottom.js
@@ -2,7 +2,7 @@ var timeleft = 100;
 var downloadTimer = setInterval(function(){
   if(timeleft <= 0){
     clearInterval(downloadTimer);
-    document.getElementById("countdown").innerHTML = "SAYEMBARBOT5758";
+    document.getElementById("countdown").innerHTML = "CANCELCHANGE93";
   } else {
     document.getElementById("countdown").innerHTML = timeleft + " seconds Code";
   }
